d6c3c38919fe51bdf3d1a9473fa1a1e6fef6743f
swali-unc <swali@live.unc.edu>
2024-03-31 11:41:47 -0400

    Change branch prediction constant

--- pa4/CodeGeneration/x64/OpcodePrefix.java
+++ pa4/CodeGeneration/x64/OpcodePrefix.java
@@ -6,7 +6,7 @@ public enum OpcodePrefix {
 	REPNE(0xF2), REPNZ(0xF2),
 	REP(0xF3), REPE(0xF3), REPZ(0xF3),
 	CS(0x2E), SS(0x36), DS(0x3E), ES(0x26), // these won't work when we're in 64-bit mode
-	FS(0x64), GS(0x65), BranchNotTaken(0x3E), BranchTaken(0x3E),
+	FS(0x64), GS(0x65), BranchNotTaken(0x2E), BranchTaken(0x3E),
 	OperandSizeOverride(0x66),
 	AddressSizeOverride(0x67);
 	
